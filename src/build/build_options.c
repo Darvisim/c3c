87fffdeed0029d1b0d6957af11cb42832c7e0b8d
Darvis <dsouzavishal685@gmail.com>
2026-06-02 21:58:24 +0530

    Add dragonfly-x64 target to arch_os_target array

--- src/build/build_options.c
+++ src/build/build_options.c
@@ -2090,6 +2090,7 @@ static int parse_option_select(const char *start, unsigned count, const char **e
 const char *arch_os_target[ARCH_OS_TARGET_LAST + 1] = {
 		[ANDROID_AARCH64] = "android-aarch64",
 		[ANDROID_X86_64] = "android-x86_64",
+		[DRAGON_FLY_X64] = "dragonfly-x64",
 		[ELF_AARCH64] = "elf-aarch64",
 		[ELF_RISCV32] = "elf-riscv32",
 		[ELF_RISCV64] = "elf-riscv64",
